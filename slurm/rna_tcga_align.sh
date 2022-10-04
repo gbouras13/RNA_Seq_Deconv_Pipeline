f0ccae274614812e4564d3d711ec3e8b1253e410
gbouras13 <george.bouras@adelaide.edu.au>
2022-10-04 16:14:51 +1030

    fix slrm

--- slurm/rna_tcga_align.sh
+++ slurm/rna_tcga_align.sh
@@ -12,8 +12,8 @@
 #SBATCH -p batch
 #SBATCH -N 1                                                    # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
 #SBATCH -c 1                                                    # number of cores (sequential job calls a multi-thread program that uses 8 cores)
-#SBATCH --time=23:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
-#SBATCH --mem=150GB                                              # specify memory required per node
+#SBATCH --time=2-23:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
+#SBATCH --mem=1GB                                              # specify memory required per node
 
 
 SNK_DIR="/hpcfs/users/a1667917/Kevin/RNA_Seq_Deconv_Pipeline"
@@ -24,6 +24,6 @@ cd $SNK_DIR
 module load Anaconda3/2020.07
 conda activate snakemake_clean_env
 
-snakemake -s rna_seq_runner.smk --use-conda --config TCGA=True  Reads=/hpcfs/users/a1667917/Kevin/TCGA_RNA_Total_Bams/  Output=/hpcfs/users/a1667917/Kevin/RNA_TCGA_Out HG38_dir='/hpcfs/users/a1667917/STAR_Ref_Genomes' Salmon_dir='/hpcfs/users/a1667917/Salmon_Ref_Genomes' --profile wgs_tcga
+snakemake -s rna_seq_runner.smk --use-conda --config TCGA=True  Reads=/hpcfs/users/a1667917/Kevin/HNSC_RNA_BAMS  Output=/hpcfs/users/a1667917/Kevin/RNA_TCGA_Out HG38_dir='/hpcfs/users/a1667917/STAR_Ref_Genomes' Salmon_dir='/hpcfs/users/a1667917/Salmon_Ref_Genomes' --profile wgs_tcga
 
 conda deactivate
